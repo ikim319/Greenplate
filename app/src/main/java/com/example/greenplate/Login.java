41dbf8d6f1721aee85eed4c16e03c7cfe0f9b9c1
sidkol1 <156947180+sidkol1@users.noreply.github.com>
2024-02-25 06:27:22 -0500

    Update Login.java

--- app/src/main/java/com/example/greenplate/Login.java
+++ app/src/main/java/com/example/greenplate/Login.java
@@ -2,7 +2,10 @@ package com.example.greenplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Set up start and quit buttons
+        Button startButton = findViewById(R.id.startButton);
+        Button quitButton = findViewById(R.id.quitButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // navigate to our login
+                startActivity(new Intent(Login.this, AccountCreation.class));
+            }
+        });
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // exit our app
+                finishAffinity();
+            }
+        });
     }
 }
