efef47319e9c2f8f764baaf32b5a6e0f7288f97d
DhruvJP <DhruvJP@gmail.com>
2024-02-25 19:22:31 -0500

    Formatted proper spacing to match corresponding files

--- app/src/main/java/com/example/greenplate/Login.java
+++ app/src/main/java/com/example/greenplate/Login.java
@@ -65,7 +65,7 @@ public class Login extends AppCompatActivity {
                     Toast.makeText(Login.this, "Enter email", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                
+
                 if (TextUtils.isEmpty(password)) {
                     Toast.makeText(Login.this, "Enter password", Toast.LENGTH_SHORT).show();
                     return;
