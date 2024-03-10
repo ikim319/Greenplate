c3356ff6ddff4d1728629bf057d9f3aadff6011f
DhruvJP <DhruvJP@gmail.com>
2024-03-10 15:29:32 -0400

    Added meal sub-database in Firebase Realtime Database along with input and log functionality.

--- app/src/main/java/com/example/greenplate/views/InputMeal.java
+++ app/src/main/java/com/example/greenplate/views/InputMeal.java
@@ -6,12 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.greenplate.R;
-
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 public class InputMeal extends AppCompatActivity {
 
+
+    EditText editTextMeal, editTextCalories;
+    private DatabaseReference rootDatabref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +30,19 @@ public class InputMeal extends AppCompatActivity {
         Button buttonShoppingList = findViewById(R.id.shoppinglist);
         Button buttonBackWelcome = findViewById(R.id.Logout);
         Button buttonPersonalInfo = findViewById(R.id.PInformation);
+        Button buttonLog = findViewById(R.id.btn_Log);
+
+        editTextMeal = findViewById(R.id.editTextMeal);
+        editTextCalories = findViewById(R.id.editTextCalories);
+
+        rootDatabref = FirebaseDatabase.getInstance().getReference().child("Meal");
+
+        buttonLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveMeal();
+            }
+        });
 
         buttonHome.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -63,6 +84,16 @@ public class InputMeal extends AppCompatActivity {
         });
 
     }
+
+    private void saveMeal() {
+        String Meal = editTextMeal.getText().toString();
+        String Calories = editTextCalories.getText().toString();
+
+        Meal meal = new Meal(Meal, Calories);
+        rootDatabref.push().setValue(meal);
+        Toast.makeText(InputMeal.this, "Saved successfully!", Toast.LENGTH_SHORT).show();
+    }
+
 
     /*
     method for updating the height, weight, and gender displayed on InputMeal
