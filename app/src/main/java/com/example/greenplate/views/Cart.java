494bc4ab9192bd7b87677bb60e10f16aa7cfb141
DhruvJP <DhruvJP@gmail.com>
2024-04-20 12:28:46 -0400

    Basic functions for cart

--- app/src/main/java/com/example/greenplate/views/Cart.java
+++ app/src/main/java/com/example/greenplate/views/Cart.java
@@ -1,21 +1,66 @@
 package com.example.greenplate.views;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.example.greenplate.R;
+import com.example.greenplate.model.ShoppingListModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 
 public class Cart extends AppCompatActivity {
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    MyAdapter myAdapter;
+    ArrayList<ShoppingListModel> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        recyclerView = findViewById(R.id.cartList);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        database = FirebaseDatabase.getInstance().getReference("ShoppingList");
+        list = new ArrayList<>();
+        myAdapter = new MyAdapter(this, list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //list.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    ShoppingListModel shop = dataSnapshot.getValue(ShoppingListModel.class);
+                    list.add(shop);
+                }
+                myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Cart.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //Nav Bar Button instantiation
         Button buttonHome = findViewById(R.id.btn_Home);
@@ -26,11 +71,13 @@ public class Cart extends AppCompatActivity {
         Button buttonPersonalInfo = findViewById(R.id.PInformation);
         Button buttonShoppingList = findViewById(R.id.shoppinglist);
 
+
         buttonHome.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(Cart.this, Home.class));
             }
         });
+
 
         buttonRecipe.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -39,12 +86,14 @@ public class Cart extends AppCompatActivity {
             }
         });
 
+
         buttonIngredients.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // navigate to our login
                 startActivity(new Intent(Cart.this, Ingredients.class));
             }
         });
+
 
         buttonInputMeal.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -53,6 +102,7 @@ public class Cart extends AppCompatActivity {
             }
         });
 
+
         buttonBackWelcome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,12 +110,14 @@ public class Cart extends AppCompatActivity {
             }
         });
 
+
         buttonPersonalInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Cart.this, PersonalInformation.class));
             }
         });
+
 
         buttonShoppingList.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
